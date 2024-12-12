afa6440ed3a084b1936a6ba9176c0f08dfed7b65
krysxuu <ex2023@nyu.edu>
2024-12-12 10:54:55 +0800

    Update rainy.js

--- final x 2/rainy/rainy.js
+++ final x 2/rainy/rainy.js
@@ -4,7 +4,7 @@ let rainyMusic;
 function preload() {
     rainyMusic = loadSound("assets/Rainy.mp3", 
         () => console.log("Sound loaded successfully!"),
-        (err) => console.error("Failed to load sound:", err)
+        (err) => console.error("Error loading sound:", err)
     );
 }
 
@@ -13,9 +13,8 @@ function setup() {
     canvas.parent('p5-canvas-container');
     raindrop = new Raindrop(300, 300);
 
-    // Add a start button for audio (in case autoplay is blocked)
     let startButton = createButton("Start Rain Sound");
-    startButton.position(width / 2 - 50, height + 20);
+    startButton.position(10, height + 20);
     startButton.mousePressed(() => {
         if (!rainyMusic.isPlaying()) {
             rainyMusic.play();
@@ -30,8 +29,7 @@ function draw() {
 }
 
 function keyPressed() {
-    if (keyCode === LEFT_ARROW || keyCode === RIGHT_ARROW || 
-        keyCode === UP_ARROW || keyCode === DOWN_ARROW) {
+    if ([LEFT_ARROW, RIGHT_ARROW, UP_ARROW, DOWN_ARROW].includes(keyCode)) {
         if (!rainyMusic.isPlaying()) {
             rainyMusic.play();
         }
@@ -39,7 +37,7 @@ function keyPressed() {
 }
 
 function keyReleased() {
-    if (!keyIsPressed) { // Simplified logic
+    if (!keyIsPressed) {
         rainyMusic.stop();
     }
 }
@@ -58,29 +56,17 @@ class Raindrop {
         noStroke();
         beginShape();
         vertex(0, -this.size / 2);
-        bezierVertex(this.size / 4, -this.size / 3,
-                     this.size / 4, this.size / 3,
-                     0, this.size / 2);
-        bezierVertex(-this.size / 4, this.size / 3,
-                     -this.size / 4, -this.size / 3,
-                     0, -this.size / 2);
+        bezierVertex(this.size / 4, -this.size / 3, this.size / 4, this.size / 3, 0, this.size / 2);
+        bezierVertex(-this.size / 4, this.size / 3, -this.size / 4, -this.size / 3, 0, -this.size / 2);
         endShape(CLOSE);
         pop();
     }
 
     move() {
-        if (keyIsDown(LEFT_ARROW)) {
-            this.x -= 5;
-        }
-        if (keyIsDown(RIGHT_ARROW)) {
-            this.x += 5;
-        }
-        if (keyIsDown(UP_ARROW)) {
-            this.y -= 5;
-        }
-        if (keyIsDown(DOWN_ARROW)) {
-            this.y += 5;
-        }
+        if (keyIsDown(LEFT_ARROW)) this.x -= 5;
+        if (keyIsDown(RIGHT_ARROW)) this.x += 5;
+        if (keyIsDown(UP_ARROW)) this.y -= 5;
+        if (keyIsDown(DOWN_ARROW)) this.y += 5;
 
         this.x = constrain(this.x, 0, width);
         this.y = constrain(this.y, 0, height);
